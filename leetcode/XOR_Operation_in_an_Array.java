c7159e88378f543e2234e289603a4b5ae6152675
sowmyag07 <sowmyagodi07@gmail.com>
2026-08-27 20:00:21 +0530

    Added LeetCode solution: XOR Operation in an Array

---
+++ leetcode/XOR_Operation_in_an_Array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int xorOperation(int n, int start) {
+        int result = 0;
+        int[] nums = new int[n];
+        for(int i = 0; i< nums.length;i++){
+            nums[i] = start + 2 *i;
+            result = result ^ nums[i];
+        }
+        return result;
+    }
+}

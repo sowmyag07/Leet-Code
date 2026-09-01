b9ed314ac5a775d15ca01f942cd61a4ca74116f1
sowmyag07 <sowmyagodi07@gmail.com>
2026-09-01 07:53:57 +0530

    Added LeetCode solution: Find the Index of the First Occurrence in a String

---
+++ leetcode/Find_the_Index_of_the_First_Occurrence_in_a_String.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        for(int i  = 0; i <=haystack.length()-needle.length();i++){
+            if(haystack.startsWith(needle,i)){
+                return i;
+            }
+
+        }
+        return -1;
+    }
+}

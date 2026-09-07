dd80d8dc7d8309b8affeca22cc0533104c496c0d
sowmyag07 <sowmyagodi07@gmail.com>
2026-09-07 15:49:47 +0530

    Added LeetCode solution: Intersection of Two Arrays II

---
+++ leetcode/Intersection_of_Two_Arrays_II.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        // Count frequencies of nums1
+        for (int num : nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        
+        ArrayList<Integer> result = new ArrayList<>();
+        
+        // Check nums2
+        for (int num : nums2) {
+            if (map.containsKey(num) && map.get(num) > 0) {
+                result.add(num);
+                map.put(num, map.get(num) - 1);
+            }
+        }
+        
+        // Convert ArrayList<Integer> to int[]
+        int[] ans = new int[result.size()];
+        
+        for (int i = 0; i < result.size(); i++) {
+            ans[i] = result.get(i);
+        }
+        
+        return ans;
+    }
+}
